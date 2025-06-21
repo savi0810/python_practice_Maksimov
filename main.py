96ab133b303d358e076af52d1cad6d5c68b7ea4d
savi0810 <alex08max2007@gmail.com>
2025-06-21 17:48:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-import sys
 from form_manager import FormManager
 from constants import COMMAND_NAME, INVALID_COMMAND
 
@@ -7,18 +6,26 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('command', type=str)
     args, unknown = parser.parse_known_args()
-    
     if args.command != COMMAND_NAME:
         print(INVALID_COMMAND)
-        sys.exit(1)
+        return
     
-    # Парсинг параметров вида --ключ=значение
     query_params = {}
+    current_key = None
     for arg in unknown:
         if arg.startswith('--'):
+            if current_key is not None:
+                query_params[current_key] = " ".join(query_params[current_key])
+            
             key_value = arg[2:].split('=', 1)
-            if len(key_value) == 2:
-                query_params[key_value[0]] = key_value[1]
+            current_key = key_value[0]
+            query_params[current_key] = [key_value[1]] if len(key_value) > 1 else []
+        else:
+            if current_key is not None:
+                query_params[current_key].append(arg)
+    
+    if current_key is not None:
+        query_params[current_key] = " ".join(query_params[current_key])
     
     manager = FormManager()
     result = manager.process_query(query_params)
